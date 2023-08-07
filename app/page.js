038cb8d9ab6a77ffb1db6388a474072912d928ba
Sars4 <logansars225@gmail.com>
2023-08-06 23:21:35 -0400

    new html and css

--- app/page.js
+++ app/page.js
@@ -3,24 +3,55 @@ import styles from './page.module.css'
 
 export default async function Home() {
   return (
-    <html>
-      <head>
-        <title>Logan Sarsfield</title>
-      </head>
-    <body>
-      <header>
-        <h1>Hello World</h1>
-        <hr></hr><br></br>
-      </header>
+    <html lang="en">
+<head>
+  <meta charset="UTF-8"></meta>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+  <link rel="stylesheet" href="styles.css"></link>
+  <title>My Personal Homepage</title>
+</head>
+<body>
+  <header class="header">
+    <h1 class="name">Logan Sarsfield</h1>
+    <p class="tagline"> Major in Data Science</p>
+  </header>
 
-      <main>
-        <p>My name is Logan Sarsfield, and I am a senior at Indiana University majoring in computer science with a focus in data science, and minoring in marketing.
-        I 
-        </p>
-        <hr></hr>
-      </main>
+  <section class="about">
+    <h2>About Me</h2>
+    <p>My name is Logan Sarsfield, and I am a senior at Indiana University majoring in computer science with a focus in data science, and minoring in marketing.
+        I </p>
+  </section>
 
-    </body>
-  </html>
+  <section class="portfolio">
+    <h2>Portfolio</h2>
+    <div class="project">
+      <img src="project1.jpg" alt="Project 1"></img>
+      <h3>Project 1</h3>
+      <p>Description</p>
+    </div>
+    <div class="project">
+      <img src="project2.jpg" alt="Project 2"></img>
+      <h3>Project 2</h3>
+      <p>Description</p>
+    </div>
+  </section>
+
+  <section class="about">
+    <h2>  Experience</h2>
+    <ul>
+    <li><h3>Sales Representitive</h3></li>
+    <p class="indent">Description</p>
+    <li><h3>Sales Representitive</h3></li>
+    <p class="indent">Description</p>
+    <li><h3>Sales Representitive</h3></li>
+    <p class="indent">Description</p>
+    </ul>
+  </section>
+
+  <footer class="footer">
+    <p>&copy; 2023 Logan Sarsfield. All rights reserved.</p>
+  </footer>
+</body>
+</html>
   )
 }
